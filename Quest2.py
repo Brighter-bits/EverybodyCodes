254a05ddc1069346320452d21a39077f465542f4
Brighter-bits <85905618+Brighter-bits@users.noreply.github.com>
2025-01-20 09:20:42 +0000

    Did part 2, I'm slowing down a lot, part 3 looks evil

--- Quest2.py
+++ Quest2.py
@@ -1,35 +1,53 @@
 import regex as re
-with open("input2A.txt", "r") as f:
-    Runes = f.readline().lstrip("WORDS:").rstrip("\n").split(",")
-    for i in range(len(Runes)):
-        if "." in Runes[i]:
-            Runes[i] = Runes[i].replace(".", "\\.")
-    f.readline()
-    Helmet = f.readline()
-    total = 0
-    for rune in Runes:
-        Power = re.findall(f'{rune}', Helmet)
-        total += len(Power)
-    print(total)
+def P1():
+    with open("input2A.txt", "r") as f:
+        Runes = f.readline().lstrip("WORDS:").rstrip("\n").split(",")
+        for i in range(len(Runes)):
+            if "." in Runes[i]:
+                Runes[i] = Runes[i].replace(".", "\\.")
+        f.readline()
+        Helmet = f.readline()
+        total = 0
+        for rune in Runes:
+            Power = re.findall(f'{rune}', Helmet)
+            total += len(Power)
+        print(total)
 
 
 
-
-with open("input2B.txt", "r") as f:
-    Runes = f.readline().lstrip("WORDS:").rstrip("\n").split(",")
-    for i in range(len(Runes)):
-        if "." in Runes[i]:
-            Runes[i] = Runes[i].replace(".", "\\.")
-        Runes.append(Runes[i][::-1])
-    f.readline()
-    Shield = list(map(lambda x: x.replace("\n", ""), f.readlines()))
-    for rune in Runes:
-        for line in range(len(Shield)):
-            Shield[line] = re.sub(f'{rune}', f"{rune[:-1]}#{rune[-1]}", Shield[line])
-    line = ''.join(Shield)
-    print(len(re.findall(r'#', line)))
+def P2():
+    with open("input2B.txt", "r") as f:
+        Runes = f.readline().replace("WORDS:", "").rstrip("\n").split(",")
+        for i in range(len(Runes)):
+            Runes.append(Runes[i][::-1])
+        f.readline()
+        print(Runes)
+        Shield = list(map(lambda x: x.replace("\n", ""), f.readlines()))
+        UpAndDown = list(Shield)
+        for i in range(len(UpAndDown)):
+            UpAndDown[i] = re.sub(r"[\w\W]", ".", UpAndDown[i])
+        # print(UpAndDown)
+        for i in range(len(Shield)):
+            # breakpoint()
+            Ranges = []
+            for Rune in Runes:
+                for Match in re.finditer(f'{Rune}', Shield[i], overlapped=True):
+                    Ranges.append([Match.start(0), Match.end(0)])
+            for Range in Ranges:
+                for j in range(Range[0], Range[1]):
+                    UpAndDown[i] = UpAndDown[i][:j] + '#' + UpAndDown[i][j+1:]
+            # breakpoint()
+        # print(UpAndDown)
+        total = 0
+        for line in UpAndDown:
+            total += line.count("#")
+            print(len(line))
+        print(total)
+                    
 
 
 
 with open("input2C.txt", "r") as f:
     pass
+
+P2()
