7e12c40b8bb94e5eebd1725097bbaa8a90d1a0ed
Brighterbits <richardrazzell@icloud.com>
2025-02-08 19:53:47 +0000

    Done in 11mins and 2 seconds. I feel like a god

---
+++ Quest4.py
@@ -0,0 +1,16 @@
+Parts = ["everybody_codes_e2024_q04_p1.txt", "everybody_codes_e2024_q04_p2.txt", "everybody_codes_e2024_q04_p3.txt"]
+
+
+with open(Parts[1], "r") as f:
+    nails = f.readlines()
+    nails = list(map(int, list(map(lambda x: x.replace("\n", ""), nails))))
+    Target = min(nails)
+    print(sum(list(map(lambda x: x-Target, nails))))
+
+with open(Parts[2], "r") as f:
+    nails = f.readlines()
+    nails = list(map(int, list(map(lambda x: x.replace("\n", ""), nails))))
+    import math
+    Target = sorted(nails)[math.floor(len(nails)/2)]
+    print(sum(list(map(lambda x: abs(x-Target), nails))))
+    
