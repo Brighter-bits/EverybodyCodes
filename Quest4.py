d5bb16d6fd96d32c2119f954ffaa5d4ad6415e57
Brighterbits <richardrazzell@icloud.com>
2025-02-08 19:54:25 +0000

    Just changed the code slightly to it outputs all results

--- Quest4.py
+++ Quest4.py
@@ -1,11 +1,11 @@
 Parts = ["everybody_codes_e2024_q04_p1.txt", "everybody_codes_e2024_q04_p2.txt", "everybody_codes_e2024_q04_p3.txt"]
 
-
-with open(Parts[1], "r") as f:
-    nails = f.readlines()
-    nails = list(map(int, list(map(lambda x: x.replace("\n", ""), nails))))
-    Target = min(nails)
-    print(sum(list(map(lambda x: x-Target, nails))))
+for i in range(2):
+    with open(Parts[i], "r") as f:
+        nails = f.readlines()
+        nails = list(map(int, list(map(lambda x: x.replace("\n", ""), nails))))
+        Target = min(nails)
+        print(sum(list(map(lambda x: x-Target, nails))))
 
 with open(Parts[2], "r") as f:
     nails = f.readlines()
