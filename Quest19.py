8506baf1b42ef60003ee95e25d601cf0b4d5b5d0
Brighterbits <richardrazzell@icloud.com>
2025-05-14 01:27:04 +0100

    Second part not working, I will look at it tomorrow.

--- Quest19.py
+++ Quest19.py
@@ -7,19 +7,26 @@ def Spin(dir, Piece):
         return [[Piece[0][1], Piece[0][2], Piece[1][2]], [Piece[0][0], Piece[1][1], Piece[2][2]], [Piece[1][0], Piece[2][0], Piece[2][1]]]
     else:
         raise Exception("What are you doing?")
-def Solve():
-    with open(Parts[0]) as f:
-        inp = list(map(lambda x: list(x.replace("\n", "")), f.readlines()))
-        instructions = inp[0]
-        grid = inp[2:]
-        length = len(inp[2])
-        for i in range(length-2):
-            Piece = [grid[0][i:i+3], grid[1][i:i+3], grid[2][i:i+3]]
-            Piece = Spin(cycle(instructions)[i], Piece)
-            for a in range(3):
-                for j in range(3):
-                    grid[a][i+j] = Piece[a][j]
-        print(grid)
-        
 
-Solve()
+def Solve(part):
+    with open(Parts[part-1]) as f:
+        inp = list(map(lambda x: list(x.replace("\n", "")), f.readlines()))
+        instructions = cycle(inp[0])
+        grid = inp[2:]
+        length = len(grid[2])
+        for times in range(1 if part == 1 else 1000):
+            for row in range(len(grid)-2):
+                for i in range(length-2):
+                    Piece = [grid[row][i:i+3], grid[row+1][i:i+3], grid[row+2][i:i+3]]
+                    Piece = Spin(next(instructions), Piece)
+                    for a in range(3):
+                        for j in range(3):
+                            grid[row+a][i+j] = Piece[a][j]
+        for line in grid:
+            # if ">" in line and "<" in line:
+                # import regex as re
+                # print("".join(list(re.findall(r'>(.*?)<', "".join(line)))))
+            print("".join(line))
+
+
+Solve(2)
