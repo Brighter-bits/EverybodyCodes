99783c2264dba44685af50f93c26a37e97e2bd36
Brighterbits <richardrazzell@icloud.com>
2025-11-08 23:25:34 +0000

    Oops, forgot to commit quest 5

---
+++ [2025] The Song of Ducks and Dragons/Quest5.py
@@ -0,0 +1,160 @@
+Parts = ["everybody_codes_e2025_q05_p1.txt", "everybody_codes_e2025_q05_p2.txt", "everybody_codes_e2025_q05_p3.txt"]
+from collections import defaultdict
+class leaf:
+    left = -1
+    right = -1
+    middle = -1
+    down = -1
+    FirstOrder = -1
+    SecondOrder = []
+    ThirdOrder = -1
+    def __init__(self, id=-1):
+        self.ThirdOrder = id+1
+    def Attach(self, num):
+        if self.middle == -1:
+            self.middle = num
+        elif num < self.middle and self.left == -1:
+            self.left = num
+        elif num > self.middle and self.right == -1:
+            self.right = num
+        elif self.down == -1:
+            self.down = leaf()
+            self.down.Attach(num)
+        else:
+            self.down.Attach(num)
+    def Reveal(self):
+        if self.down == -1:
+            return str(self.middle)
+        else:
+            return str(self.middle) + str(self.down.Reveal())
+    def Sideways(self):
+        answer = ""
+        if self.left != -1:
+            answer += str(self.left)
+        answer += str(self.middle)
+        if self.right != -1:
+            answer += str(self.right)
+        if self.down == -1:
+            return answer
+        else:
+            return answer + "," + self.down.Sideways()
+
+def compare(a:leaf, b:leaf):
+    counter = 0
+    AOrder = list(map(int, a.SecondOrder.split(",")))
+    BOrder = list(map(int, b.SecondOrder.split(",")))
+    while True:
+        try:
+            if AOrder[counter] > BOrder[counter]:
+                return [a, b]
+            elif AOrder[counter] < BOrder[counter]:
+                return [b, a]
+            counter += 1
+
+        except:
+            return -1
+
+def Solve1n2(part):
+    with open(Parts[part-1]) as f:
+        inp = list(map(lambda x: x.split(":")[1].replace("\n", "").split(","), f.readlines()))
+        for i in range(len(inp)):
+            inp[i] = list(map(int, inp[i]))
+        boneyboys:list[leaf] = []
+        for i in range(len(inp)):
+            Start = leaf(i)
+            for i in inp[i]:
+                Start.Attach(i)
+            boneyboys.append(Start)
+        if part == 1:
+            print(Start.FirstOrder)
+        else:
+            boneyboys = list(map(lambda x: x.FirstOrder, ))
+            print(max())
+
+
+def Solve3():
+    with open(Parts[2]) as f:
+        inp = list(map(lambda x: x.split(":")[1].replace("\n", "").split(","), f.readlines()))
+        for i in range(len(inp)):
+            inp[i] = list(map(int, inp[i]))
+        boneyboys:list[leaf] = []
+        for i in range(len(inp)):
+            Start = leaf(i)
+            for i in inp[i]:
+                Start.Attach(i)
+            Start.FirstOrder = Start.Reveal()
+            Start.SecondOrder = Start.Sideways()
+            boneyboys.append(Start)
+        FinalDict = defaultdict(list)
+        for bone in boneyboys:
+            FinalDict[bone.FirstOrder].append(bone)
+        results = sorted(list(FinalDict.items()), reverse=True, key=lambda x: int(x[0]))
+        for i in range(len(results)):
+            if len(results[i][1]) > 1:
+                answer = ""
+                if len(results[i][1]) == 2:
+                    answer = compare(results[i][1][0], results[i][1][1])
+                    if answer == -1:
+                        answer = sorted(results[i][1],reverse=True, key= lambda x: x.ThirdOrder)
+                else: # If statement gore
+                    A, B, C = results[i][1][0], results[i][1][1], results[i][1][2]
+                    if compare(A, B) == -1:
+                        if compare(B, C) == -1:
+                            answer = sorted([A, B, C], reverse=True, key=lambda x: x.ThirdOrder)
+                        elif compare(B, C) == [C, B]:
+                            answer = [C]
+                            answer.extend(sorted([A, B], reverse=True, key=lambda x: x.ThirdOrder))
+                        elif compare(B, C) == [B, C]:
+                            answer = sorted([A, B], reverse=True, key=lambda x: x.ThirdOrder)
+                            answer.append(C)
+                    elif compare(A, B) == [A, B]:
+                        if compare(B, C) == -1:
+                            answer = [A]
+                            answer.extend(sorted([B, C], reverse=True, key=lambda x: x.ThirdOrder))
+                        elif compare(B, C) == [B, C]:
+                            answer = [A, B, C]
+                        elif compare(B, C) == [C, B]:
+                            if compare(A, C) == -1:
+                                answer = sorted([A, C], reverse=True, key=lambda x: x.ThirdOrder)
+                                answer.append(B)
+                            elif compare(A, C) == [A, C]:
+                                answer = [A, C, B]
+                            elif compare(C, A) == [C, A]:
+                                answer = [C, A, B]
+                    elif compare(A, B) == [B, A]:
+                        if compare(A, C) == -1:
+                            answer = [B]
+                            answer.extend(sorted([A, C], reverse=True, key=lambda x: x.ThirdOrder))
+                        elif compare(A, C) == [A, C]:
+                            answer = [B, A, C]
+                        elif compare(A, C) == [C, A]:
+                            if compare(B, C) == -1:
+                                answer = sorted([B, C], reverse=True, key=lambda x: x.ThirdOrder)
+                                answer.append(A)
+                            elif compare(B, C) == [B, C]:
+                                answer = [B, C, A]
+                            elif compare(B, C) == [C, B]:
+                                answer = [C, B, A]
+                results[i] = (results[i][0], answer)
+        checksum = 0
+        counter = 1
+        for i in range(len(results)):
+            if len(results[i][1]) == 1:
+                checksum += (counter) * results[i][1][0].ThirdOrder
+            if len(results[i][1]) == 2:
+                checksum += (counter) * results[i][1][0].ThirdOrder
+                counter += 1
+                checksum += (counter) * results[i][1][1].ThirdOrder
+            if len(results[i][1]) == 3:
+                checksum += (counter) * results[i][1][0].ThirdOrder
+                counter += 1
+                checksum += (counter) * results[i][1][1].ThirdOrder
+                counter += 1
+                checksum += (counter) * results[i][1][2].ThirdOrder
+            counter += 1
+        print(checksum)
+
+        
+
+# Solve(1)
+# Solve(2)
--- [2025] The Song of Ducks and Dragons/README.md
+++ [2025] The Song of Ducks and Dragons/README.md
@@ -18,12 +18,14 @@ negative fractional number, instead of just ignoring the fraction, it goes to th
 It makes sense, but I'm still a bit annoyed that I could have just used a single different function and would have done way better. I guess this is how you learn. I'M COMING FOR YOU TONYA!!! (For reference,
 this is the person now heading the UK, I **will** win... just give me time... maybe... unless the next puzzles are really difficult... then maybe not...)
 
-Wow, I was clearly very tired yesterday, because I didn't even make the code spit out every single part correctly.
 
 # Day 3
 
+Wow, I was clearly very tired yesterday, because I didn't even make the code spit out every single part correctly.
+
 Another easy one, either I've gotten better, or the challenges are easier than last year. For the sake of my ego, I have decided it's the former. Once I understood what was going on, it was quite easy to
 handle.
+
 
 # Day 4
 
@@ -31,3 +33,11 @@ Geeze, I'm so sleepy that I can't do basic maths... This took me far longer than
 over and over, instead of finding the proper differences across the entire list.
 
 Do not code while sleepy, bad idea.
+
+# Day 5
+
+Oops, got distracted by a pokemon game... probably not a good day to do that on, given this day was pretty challenging. I think it was a really unique puzzle, and I actually really enjoyed doing it. I didn't
+have any headbanging errors, just a few that I managed to rubber duck my way through. Overall, nice and fun.
+
+Oh my god, how do people do this puzzle that fast?? Ahhh, so, since all the SecondOrder items are going to have the same number of levels, you can just have a list of [quality, first level, second level, ..., id].
+Instead of doing whatever the hell I just did. Touché, but now I've been knocked down once again. I must reclaim my throne (unlikely as it is, as stuff only gets harder from here.)
